1c30cc36baae18649e232997c63b2100c30fb7e6
NirmalNagaraj <nirmal070308@gmail.com>
2025-02-16 12:01:42 +0530

    add: Implemented the modulo turn algorithm in the /utils file, which returns the final direction after the implementation of the commands

--- utils.js
+++ utils.js
@@ -3,21 +3,29 @@ function isValidPosition(position, grid) {
     return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] === 0;
 }
 
-
-function isMove(instructions){
-
+function isTurn(instruction) {
+    return instruction === 'L' || instruction === 'R';
 }
-function isTurn(instructions){
 
-}
-function triggerMove(instructions){
-
-}
-function triggerTurn(instructions){
-
+function isMove(instruction) {
+    return instruction === 'F' || instruction === 'B';
 }
 
 
-module.exports = { isValidPosition, isMove , isTurn , triggerMove,triggerTurn };
 
+function triggerTurn(instruction, initialDirection) {
+    const directionMap = { 'N': 0, 'E': 1, 'S': 2, 'W': 3 };
+    const reverseDirectionMap = ['N', 'E', 'S', 'W'];
+    if (!isTurn(instruction)) {
+        throw new Error("Invalid turn instruction. Must be 'L' or 'R'.");
+    }
 
+    let direction = directionMap[initialDirection];
+
+    // Modulo operation of Turn Algorithm
+    direction = (instruction === 'L') ? (direction + 3) % 4 : (direction + 1) % 4;
+
+    return reverseDirectionMap[direction];
+}
+
+module.exports = { isValidPosition, isMove, isTurn, triggerTurn };
