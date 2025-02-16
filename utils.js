612a36d9e4e90019bfd5dd9af162b267412fcfc6
jeevareka296@gmail.com <jeevareka296@gmail.com>
2025-02-16 17:11:25 +0530

    created isMove,triggerMove and made some changes to exceute

--- utils.js
+++ utils.js
@@ -1,5 +1,5 @@
 function isValidPosition(position, grid) {
-    const [x, y] = position;
+    const [x, y, direction] = position;
     return x >= 0 && x < grid.length && y >= 0 && y < grid[0].length && grid[x][y] === 0;
 }
 
@@ -11,7 +11,26 @@ function isMove(instruction) {
     return instruction === 'F' || instruction === 'B';
 }
 
-
+function triggerMove(instruction,grid,roverPos){
+    let resPos=roverPos;
+    let direction=roverPos[2];
+    if(instruction==='F'){
+        if (direction==='N')resPos[0]++;
+        else if (direction==='S')resPos[0]--;
+        else if (direction==='E')resPos[1]++;
+        else if (direction==='W')resPos[1]--;
+    }
+    else{
+        if (direction==='N')resPos[0]--;
+        else if (direction==='S')resPos[0]++;
+        else if (direction==='E')resPos[1]--;
+        else if (direction==='W')resPos[1]++;
+    }
+    //to validate position and wrap the updated position if needed
+    resPos=wrapPosition(resPos,grid.length,grid[0].length);
+    if(!isValidPosition(resPos,grid)) return roverPos;
+    return resPos;
+}
 
 function triggerTurn(instruction, initialDirection) {
     const directionMap = { 'N': 0, 'E': 1, 'S': 2, 'W': 3 };
@@ -28,4 +47,4 @@ function triggerTurn(instruction, initialDirection) {
     return reverseDirectionMap[direction];
 }
 
-module.exports = { isValidPosition, isMove, isTurn, triggerTurn };
+module.exports = { isValidPosition, isMove, isTurn, triggerTurn,triggerMove };
